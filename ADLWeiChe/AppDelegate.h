86be05f6c1d479675e3449672e06e997e6ac3219
AndiBoyan <andi.boyan@qq.com>
2015-06-03 14:02:12 +0800

    Initial Commit

---
+++ ADLWeiChe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADLWeiChe
+//
+//  Created by icePhoenix on 15/6/3.
+//  Copyright (c) 2015年 aodelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ADLWeiChe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADLWeiChe
+//
+//  Created by icePhoenix on 15/6/3.
+//  Copyright (c) 2015年 aodelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
